167293c4849367fa11c882db2d9ec34154fa3367
SeallEgg <seallegg@gmail.com>
2026-01-12 17:07:02 -0300

    exclude unwanted packages

--- modules/desktop/environment/plasma.nix
+++ modules/desktop/environment/plasma.nix
@@ -14,5 +14,12 @@ in {
   config = mkIf cfg.enable {
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      plasma-browser-integration
+      konsole
+      elisa
+      kwallet
+      khelpcenter
+    ];
   };
 }
