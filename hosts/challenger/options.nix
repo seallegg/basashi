2766e059bd755c7cb49ac06f2aac8b4ad09e50b8
SeallEgg <seallegg@gmail.com>
2026-01-14 17:33:44 -0300

    update challenger host config

--- hosts/challenger/options.nix
+++ hosts/challenger/options.nix
@@ -3,6 +3,25 @@
     core = {
       username = "seal";
       amdgpu.enable = true;
+      git.name = "SeallEgg";
+      git.email = "seallegg@gmail.com";
+    };
+
+    desktop = {
+      environment = {
+        plasma.enable = true;
+        niri.enable = true;
+        rofi.enable = true;
+      };
+      apps = {
+        gaming.enable = true;
+      };
+    };
+
+    services = {
+      sddm.enable = true;
+      swww.enable = true;
+      flatpak.enable = true;
     };
   };
 }
--- hosts/challenger/system.nix
+++ hosts/challenger/system.nix
@@ -1,4 +1,15 @@
 {
   system = "x86_64-linux";
   arch = "znver3";
+  monitors = [
+    {
+      name = "DP-1";
+      res = "2560x1440@170.001";
+      pos = {
+        x = 0;
+        y = 0;
+      };
+      scale = 1;
+    }
+  ];
 }
