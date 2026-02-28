63d68bb5b394363bdb5e81fd37d85ecd128c146b
SeallEgg <seallegg@gmail.com>
2026-02-28 15:39:27 -0300

    Update networkmanager.nix

--- modules/services/networkmanager.nix
+++ modules/services/networkmanager.nix
@@ -1,3 +1,32 @@
 {
-  networking.networkmanager.enable = true;
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.cfg.services.networkmanager;
+in {
+  options.cfg.services.networkmanager.enable = mkEnableOption "NetworkManager";
+  config = mkIf cfg.enable {
+    programs.nm-applet.enable = true;
+    networking = {
+      networkmanager = {
+        enable = true;
+        wifi = {
+          macAddress = "stable";
+          backend = "iwd";
+        };
+        ethernet.macAddress = "stable";
+        dns = "systemd-resolved";
+      };
+      wireless.iwd.enable = true;
+      dhcpcd.enable = false;
+    };
+    services.resolved.enable = true;
+    systemd.services.ModemManager.enable = false;
+
+    users.users.${config.cfg.core.username} = {
+      extraGroups = ["networkmanager"];
+    };
+  };
 }
