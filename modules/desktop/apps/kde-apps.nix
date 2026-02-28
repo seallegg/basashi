35d937ae62963facdd58a1e10a692e0b62693d89
SeallEgg <seallegg@gmail.com>
2026-02-28 15:36:44 -0300

    fix dolphin file associations, add kdeglobals

--- modules/desktop/apps/kde-apps.nix
+++ modules/desktop/apps/kde-apps.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   lib,
-  inputs,
   config,
   ...
 }: let
@@ -9,20 +8,35 @@
   cfg = config.cfg.desktop.environment;
 in {
   config = mkIf cfg.niri.enable or cfg.plasma.enable {
-    environment.systemPackages = with pkgs; [
-      kdePackages.qtsvg
-      kdePackages.kio
-      kdePackages.kio-fuse
-      kdePackages.kio-admin
-      kdePackages.kio-extras
-      kdePackages.dolphin-plugins
-    ];
-    hj.packages = with pkgs; [
-      kdePackages.dolphin
-      kdePackages.gwenview
-      kdePackages.ark
-      kdePackages.filelight
-      kdePackages.ktorrent
-    ];
+    environment = {
+      systemPackages = with pkgs; [
+        kdePackages.qtsvg
+        kdePackages.kio
+        kdePackages.kio-fuse
+        kdePackages.kio-admin
+        kdePackages.kio-extras
+        kdePackages.dolphin-plugins
+      ];
+      # Fix dolphin file associations. How hideous
+      etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+    };
+    hj = {
+      packages = with pkgs; [
+        kdePackages.dolphin
+        kdePackages.gwenview
+        kdePackages.ark
+        kdePackages.filelight
+        kdePackages.ktorrent
+      ];
+      xdg.config.files."kdeglobals" = {
+        generator = lib.generators.toINI {};
+        value = {
+          General = {
+            TerminalApplication = "kitty";
+          };
+          Icons.Theme = "papirus-dark";
+        };
+      };
+    };
   };
 }
