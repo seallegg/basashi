f343d6176205aa9e81a5c315a216deb20aacb3b2
SeallEgg <seallegg@gmail.com>
2026-02-28 15:38:09 -0300

    Update options.nix

--- hosts/columbia/options.nix
+++ hosts/columbia/options.nix
@@ -1,8 +1,10 @@
 {
   cfg = {
     core = {
+      kernel = "custom";
       username = "seal";
       nvidia.enable = true;
+      networkmanager.enable = true;
       git.name = "SeallEgg";
       git.email = "seallegg@gmail.com";
       forceCompiledPkgs = {
@@ -16,7 +18,6 @@
 
     desktop = {
       environment = {
-        plasma.enable = true;
         niri.enable = true;
         rofi.enable = true;
       };
@@ -26,10 +27,11 @@
     };
 
     services = {
-      sddm.enable = true;
+      greetd.enable = true;
       swww.enable = true;
       swaync.enable = true;
       flatpak.enable = true;
+      automounting.enable = true;
       idevices.enable = true;
     };
   };
