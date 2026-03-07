944e69a6bdaf97b3fa7474e0a75670735c1eb43a
SeallEgg <seallegg@gmail.com>
2026-03-07 18:28:05 -0300

    Update g502.nix

--- modules/services/g502.nix
+++ modules/services/g502.nix
@@ -6,8 +6,9 @@
 }: {
   options.cfg.services.g502.enable = lib.mkEnableOption "g502";
   config = lib.mkIf config.cfg.services.g502.enable {
+    hardware.logitech.wireless.enable = true;
+    hardware.logitech.wireless.enableGraphical = true; # Installs Solaar and udev rules
     environment.systemPackages = with pkgs; [
-      solaar
       input-remapper
       usbutils
     ];
