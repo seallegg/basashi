376d8054662868191f1f83dfb5ffab53b172e0d0
SeallEgg <seallegg@gmail.com>
2026-03-15 21:12:20 -0300

    Update networkmanager.nix

--- modules/services/networkmanager.nix
+++ modules/services/networkmanager.nix
@@ -19,8 +19,19 @@
       wireless.iwd.enable = true;
       dhcpcd.enable = false;
     };
-    services.resolved.enable = true;
     systemd.services.ModemManager.enable = false;
+    services.resolved = {
+      enable = true;
+      settings.Resolve = {
+        DNS = [
+          "45.90.28.0#a772b8.dns.nextdns.io"
+          "2a07:a8c0::#a772b8.dns.nextdns.io"
+          "45.90.30.0#a772b8.dns.nextdns.io"
+          "2a07:a8c1::#a772b8.dns.nextdns.io"
+        ];
+        DNSOverTLS = "yes";
+      };
+    };
 
     users.users.${config.cfg.core.username} = {
       extraGroups = ["networkmanager"];
