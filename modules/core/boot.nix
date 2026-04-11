aed5b5bf33965344eb4e1e0b864818af37128c9d
SeallEgg <seallegg@gmail.com>
2026-04-11 14:09:11 -0300

    move plymouth back into boot.nix, initrd tweaks

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,23 +1,49 @@
-{config, ...}: {
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      timeout = 0; # hold space to access boot menu
-    };
-    initrd.systemd.enable = true;
-    kernelParams = map (m: "video=${m.name}:${m.res}") config.cfg.hardware.monitors;
+{
+  config,
+  lib,
+  ...
+}: {
+  options.cfg.services.plymouth.enable = lib.mkEnableOption "Plymouth";
 
-    tmp = {
-      useTmpfs = true;
-      tmpfsSize = "50%";
-      tmpfsHugeMemoryPages = "within_size"; # not sure how much of a difference this makes
-    };
-  };
-  zramSwap = {
-    enable = true;
-    algorithm = "lz4";
-  };
+  config = {
+    boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+        timeout = 0; # hold space to access boot menu
+      };
+      initrd = {
+        systemd.enable = true;
+        compressor = "zstd";
+        compressorArgs = ["-1" "-T0"];
+      };
 
-  hardware.enableRedistributableFirmware = true;
+      plymouth = lib.mkIf config.cfg.services.plymouth.enable {
+        enable = true;
+        theme = "bgrt";
+      };
+
+      kernelParams =
+        [
+          "quiet"
+          "loglevel=3"
+          "rd.udev.log_level=3"
+          "systemd.show_status=auto"
+          "vt.global_cursor_default=0"
+        ]
+        ++ (map (m: "video=${m.name}:${m.res}") config.cfg.hardware.monitors);
+
+      tmp = {
+        useTmpfs = true;
+        tmpfsSize = "50%";
+        tmpfsHugeMemoryPages = "within_size"; # not sure how much of a difference this makes
+      };
+    };
+    zramSwap = {
+      enable = true;
+      algorithm = "lz4";
+    };
+    services.lvm.enable = false; # why this is on by default is beyond me
+    hardware.enableRedistributableFirmware = true;
+  };
 }
