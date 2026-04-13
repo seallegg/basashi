ef1a434eae8c7d69340a5879ffe610254634d08e
SeallEgg <seallegg@gmail.com>
2026-04-13 11:04:49 -0300

    add niri and kitty support for matugen

--- modules/desktop/environment/matugen.nix
+++ modules/desktop/environment/matugen.nix
@@ -28,11 +28,23 @@
               input_path = "'~/.config/matugen/templates/rofi.template'";
               output_path = "'~/.config/rofi/colors.rasi'";
             };
+            "templates.kitty" = {
+              input_path = "'~/.config/matugen/templates/kitty.template'";
+              output_path = "'~/.config/kitty/colors.conf'";
+              post_hook = "'kill -SIGUSR1 $KITTY_PID'";
+            };
+            "templates.niri" = {
+              input_path = "'~/.config/matugen/templates/niri.template'";
+              output_path = "'~/.config/niri/colors.kdl'";
+              post_hook = "'niri msg action load-config-file'";
+            };
           };
         };
 
         "matugen/templates/Darkly.template".text = builtins.readFile "${dotfiles}/matugen/Darkly.template";
         "matugen/templates/rofi.template".text = builtins.readFile "${dotfiles}/matugen/rofi.template";
+        "matugen/templates/kitty.template".text = builtins.readFile "${dotfiles}/matugen/kitty.template";
+        "matugen/templates/niri.template".text = builtins.readFile "${dotfiles}/matugen/niri.template";
       };
     };
   };
