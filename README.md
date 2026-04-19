9e39bb372e22fa6fd1e6764b733d0b13584e7808
SeallEgg <seallegg@gmail.com>
2026-04-18 23:01:40 -0300

    Add discovery host (finally)

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ My personal NixOS configuration. Very much a WIP.
 - enterprise: simple config for VMs. Not really in use at the moment.
 - columbia: my desktop computer (7800X3D + 4070 Super), used for gaming and playing around with Linux. 
 - challenger: my Thinkpad L13 Gen 3 AMD, used primarily for studying in university.
-- ~~discovery~~: my homelab/NAS. Not yet built. 
+- discovery: my homelab/NAS. Also runs my ender-6. 
 
 Mostly based on the structure of [fazzi's NixOhEss configuration](https://gitlab.com/fazzi/nixohess). In fact, some files were (at least initially) stolen verbatim.
---
+++ hosts/discovery/default.nix
@@ -0,0 +1,53 @@
+{inputs, ...}: {
+  networking.hostName = "discovery";
+  networking.hostId = "7f833560";
+  system.stateVersion = "25.11";
+
+  boot = {
+    initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "sd_mod"];
+    kernelModules = ["kvm-amd"];
+  };
+  hardware.cpu.amd.updateMicrocode = true;
+
+  imports = [
+    inputs.disko.nixosModules.disko
+    ./partitioning.nix
+  ];
+
+  cfg = {
+    core = {
+      kernel = "cachy-lts";
+      zfs.enable = true;
+      username = "admin";
+      amdgpu.enable = true;
+    };
+
+    services = {
+      networking = {
+        defaultGateway = "192.168.0.1";
+        staticIP = {
+          eno1 = "192.168.0.87/24";
+          wlan0 = "192.168.5.87/24";
+        };
+        DoT.enable = true;
+        IPv6.enable = false;
+      };
+      networkFileSharing = {
+        enable = true;
+        shares = {
+          tank = "/mnt/tank";
+          fast = "/mnt/fast";
+        };
+        trustedSubnets = [
+          "192.168.0.0/24"
+          "192.168.5.0/24"
+        ];
+      };
+    };
+
+    terminal = {
+      git.name = "SeallEgg";
+      git.email = "seallegg@gmail.com";
+    };
+  };
+}
