2eb4152ac37da703145ba5537fb5ddfd1cb9ddb4
SeallEgg <seallegg@gmail.com>
2026-04-30 14:31:08 -0300

    Update powersaving.nix

--- modules/services/powersaving.nix
+++ modules/services/powersaving.nix
@@ -1,13 +1,54 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib) mkIf;
+  cfg = config.cfg.services.powersaving;
+
+  powerStateChange = pkgs.writeShellScript "power-state-change" ''
+    state=$1
+    case $state in
+      AC)
+        ${pkgs.brightnessctl}/bin/brightnessctl set 100%
+        ${pkgs.brightnessctl}/bin/brightnessctl --device='*kbd_backlight' set 100%
+        ;;
+      BATTERY)
+        ${pkgs.brightnessctl}/bin/brightnessctl set 30%
+        ${pkgs.brightnessctl}/bin/brightnessctl --device='*kbd_backlight' set 0%
+        ;;
+    esac
+  '';
 in {
   options.cfg.services.powersaving.enable = lib.mkEnableOption "power saving optimizations";
-  config = mkIf config.cfg.services.powersaving.enable {
+
+  config = mkIf cfg.enable {
+    boot.kernelParams = ["amd_pstate=active"];
+
+    services.auto-cpufreq.enable = true;
+    services.auto-cpufreq.settings = {
+      charger = {
+        governor = "performance";
+        energy_performance_preference = "performance";
+      };
+      battery = {
+        governor = "powersave";
+        energy_performance_preference = "power";
+        turbo = "never";
+      };
+    };
+
+    services.udev.extraRules = ''
+      SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${powerStateChange} AC"
+      SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${powerStateChange} BATTERY"
+    '';
+
     networking.networkmanager.wifi.powersave = config.cfg.services.networking.networkmanager.enable;
-    #powerManagement.powertop.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      powertop # for monitoring only
+      brightnessctl
+    ];
   };
 }
