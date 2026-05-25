b2a8a3f7debe8510df6a29b59f7c77a2f5e72a13
SeallEgg <seallegg@gmail.com>
2026-05-25 11:59:25 -0300

    Update nix.nix

--- modules/core/meta/nix.nix
+++ modules/core/meta/nix.nix
@@ -8,56 +8,54 @@
   inherit (builtins) mapAttrs;
   nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
 in {
-  config = {
-    nix = {
-      package = pkgs.nixVersions.git;
-      channel.enable = false;
-      registry = mapAttrs (_: flake: {inherit flake;}) inputs;
+  nix = {
+    package = pkgs.nixVersions.git;
+    channel.enable = false;
+    registry = mapAttrs (_: flake: {inherit flake;}) inputs;
 
-      inherit nixPath;
+    inherit nixPath;
 
-      settings = {
-        nix-path = nixPath;
-        flake-registry = "";
-        experimental-features = ["nix-command" "flakes"];
+    settings = {
+      nix-path = nixPath;
+      flake-registry = "";
+      experimental-features = ["nix-command" "flakes"];
 
-        allow-import-from-derivation = false;
-        auto-optimise-store = true;
-        use-xdg-base-directories = true;
-        extra-sandbox-paths = ["/var/cache/ccache"];
-        download-buffer-size = 1024 * 1024 * 1024;
-        http-connections = 50;
-        allowed-users = ["@wheel"];
-        trusted-users = ["@wheel"];
+      allow-import-from-derivation = false;
+      auto-optimise-store = true;
+      use-xdg-base-directories = true;
+      extra-sandbox-paths = ["/var/cache/ccache"];
+      download-buffer-size = 1024 * 1024 * 1024;
+      http-connections = 50;
+      allowed-users = ["@wheel"];
+      trusted-users = ["@wheel"];
 
-        accept-flake-config = true;
-        extra-substituters = [
-          "https://nix-community.cachix.org"
-          "https://cache.nixos-cuda.org"
-        ];
-        extra-trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-        ];
-      };
+      accept-flake-config = true;
+      extra-substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos-cuda.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      ];
     };
-
-    programs.ccache = {
-      enable = true;
-      cacheDir = "/var/cache/ccache";
-    };
-
-    nixpkgs.config.allowUnfree = true;
-
-    programs.nh = {
-      enable = true;
-      clean = {
-        enable = true;
-        dates = "monthly";
-        extraArgs = "--keep 10";
-      };
-    };
-
-    documentation.nixos.enable = false;
   };
+
+  programs.ccache = {
+    enable = true;
+    cacheDir = "/var/cache/ccache";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.nh = {
+    enable = true;
+    clean = {
+      enable = true;
+      dates = "monthly";
+      extraArgs = "--keep 10";
+    };
+  };
+
+  documentation.nixos.enable = false;
 }
