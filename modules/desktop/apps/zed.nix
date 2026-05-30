2f501e8a16093d556741da25c6ab10a69e62dd57
SeallEgg <seallegg@gmail.com>
2026-05-30 00:05:54 -0300

    Update zed.nix

--- modules/desktop/apps/zed.nix
+++ modules/desktop/apps/zed.nix
@@ -2,6 +2,16 @@
 let cfg = config.basashi.desktop.environment;
 in {
   config = lib.mkIf (cfg.niri.enable or cfg.plasma.enable) {
-    hj = { packages = with pkgs; [ zed-editor gemini-cli haskellPackages.nixfmt nixd ]; };
+    hj = {
+      packages = with pkgs; [
+        zed-editor
+        gemini-cli
+        claude-code
+        haskellPackages.nixfmt
+        statix
+        deadnix
+        nixd
+      ];
+    };
   };
 }
